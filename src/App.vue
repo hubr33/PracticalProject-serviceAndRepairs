2f1f00a52cb49abdf59609d9b01576c131845ce6
Hubert Radziwonka <hubert.radziwonka33@gmail.com>
2021-08-19 16:04:48 +0200

    v.0.0.1

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <form @submit.prevent="saveData">
+      <label for="bb">Auto</label>
+      <input type="text" placeholder="Wpisz serwisowae auto" v-model="auto" />
+      <button>WYSLIJ</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      auto: "",
+    };
+  },
+  methods: {
+    saveData() {
+      console.log(this.auto);
+    },
+  },
+};
+</script>
+
+<style></style>
